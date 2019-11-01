23cb572d46476cab97ae0e45854e669ff23fd84f
D@vid [WS] Ingty <46246771+davidwsingty@users.noreply.github.com>
2019-11-01 23:39:29 +0530

    Create setproxy.sh

---
+++ setproxy.sh
@@ -0,0 +1,25 @@
+
+# This script is used to set the proxy in Linux distros which uses /etc/profile
+# pass the value of $1 as the first positional parameter
+
+FILENAME1=/etc/profile
+
+if [[ -z $(grep "http_proxy=\"http://$1\"" $FILENAME1) ]]; then
+    echo "http_proxy=\"http://$1\"" >> $FILENAME1
+fi
+
+if [[ -z $(grep "https_proxy=\"https://$1\"" $FILENAME1) ]]; then
+    echo "https_proxy=\"https://$1\"" >> $FILENAME1
+fi
+
+if [[ -z $(grep "export http_proxy" $FILENAME1) ]]; then
+    echo "export http_proxy" >> $FILENAME1
+fi
+
+if [[ -z $(grep "export https_proxy" $FILENAME1) ]]; then
+    echo "export https_proxy" >> $FILENAME1
+fi
+
+source $FILENAME1
+
+
